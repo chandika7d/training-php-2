96fa083b35936f3964197b245cb341d3f0e8a80e
Chandika Nurdiansyah <chandika7d@gmail.com>
2023-05-25 22:35:41 +0700

    app config

--- Config/app.php
+++ Config/app.php
@@ -1,1 +1,12 @@
 <?php
+
+return [
+    "app_name" => "API Gojek",
+    "app_version" => "v0.0.1",
+    "author" => [
+        "name" => "Chandika Nurdiansyah",
+        "email" => "chandika7d@gmail.com",
+        "instagram" => "https://www.instagram.com/chandika7d/",
+        "linkedin" => "https://www.linkedin.com/in/caudil/",
+    ],
+];
--- Controllers/AppController.php
+++ Controllers/AppController.php
@@ -5,8 +5,10 @@ class AppController extends Controller {
     function __construct(){
     }
     function index(){
+        $app_meta = include APP_PATH . "/Config/app.php";
         return [
-            "message" => "Welcome to gocar api",
+            "message" => "Welcome to {$app_meta['app_name']} {$app_meta['app_version']}",
+            "about" => $app_meta,
             "routes" => get("router")
         ];
     }
